4d3750a76a494a0ba5525af2aaff87c59d25868e
Bhargabiam <bhargabiam@gmail.com>
2023-09-26 15:35:16 +0530

    impliment Dark Mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
 import React from "react";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./Components/NavBar";
 
 const App = () => {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"}>
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg="pink" area={"aside"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem bg="green" area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
