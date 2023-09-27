5ebe7a2eb78650490bf108642d433bf114f41eb9
Bhargabiam <bhargabiam@gmail.com>
2023-09-27 17:19:21 +0530

    Remove Duplecate Styling

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -2,6 +2,7 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGame from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGame();
@@ -12,13 +13,15 @@ const GameGrid = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
         padding="10"
         spacing={10}
       >
         {isLoading &&
           Skeleton.map((Skeleton) => (
-            <GameCardSkeleton key={Skeleton}></GameCardSkeleton>
+            <GameCardContainer key={Skeleton}>
+              <GameCardSkeleton key={Skeleton}></GameCardSkeleton>
+            </GameCardContainer>
           ))}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
