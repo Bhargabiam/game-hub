92040d6d77e0a055d814f7295cf9b48b195b44c9
Bhargabiam <bhargabiam@gmail.com>
2023-09-29 12:56:07 +0530

    create a generic data fetching code

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -5,7 +5,7 @@ import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isLoading } = useGame();
+  const { data, error, isLoading } = useGame();
 
   const Skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 
@@ -23,7 +23,7 @@ const GameGrid = () => {
               <GameCardSkeleton key={Skeleton}></GameCardSkeleton>
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
       </SimpleGrid>
