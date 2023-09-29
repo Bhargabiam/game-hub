2f5efe75e430bf16ae1375e67a4af30b5714cd10
Bhargabiam <bhargabiam@gmail.com>
2023-09-29 14:53:05 +0530

    Build platform selector

--- src/Components/GameGrid.tsx
+++ src/Components/GameGrid.tsx
@@ -19,7 +19,7 @@ const GameGrid = ({ selectedGenre }: Props) => {
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4, "2xl": 5 }}
-        padding="5"
+        padding="10px"
         spacing={3}
       >
         {isLoading &&
