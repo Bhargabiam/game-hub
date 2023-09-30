f5a5fad8cec6cbbf6ac45f7e051b589e3569fab1
Bhargabiam <bhargabiam@gmail.com>
2023-09-30 11:44:33 +0530

    Fix the issu with Chakra menus

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'aaf57ac71ca741c69493d959cd24eb4e'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "aaf57ac71ca741c69493d959cd24eb4e",
+  },
+});
